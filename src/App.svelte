e34c3438117c03a53712cc84491c395311a75c8e
Hannah Hudson <hannah.hch@gmail.com>
2020-01-10 22:49:19 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Title from './components/Title.svelte';
+	import Weather from './components/Weather.svelte';
+</script>
+
+<main>
+	<Title/>
+	<Weather/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/components/Weather.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from 'svelte';
+  
+  export let weatherData = [];
+  let latitude = 39.7392;
+  let longitude = 104.9903;
+
+  onMount(async () => {
+		const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=ab1fcdbee534c8cfce19f3c1ff919a6d`);
+    weatherData = await res.json();
+    
+    console.log(weatherData.weather[0].main)
+  });
+
+
+  
+</script>
